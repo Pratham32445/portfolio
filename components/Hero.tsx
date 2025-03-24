a39f9ee2ca2b925817d121fef4fa4427aec992b7
Pratham mehta <mehtapratham49@gmail.com>
2025-03-25 00:23:03 +0530

    added gmail

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 const Hero = () => {
@@ -11,10 +12,10 @@ const Hero = () => {
           I love to work on full stack projects,Freelance Projects <br/> and to build
           a robust applications i have worked on <br/> several freelance projects
         </p>
-        <div className="px-2 py-2 mt-2 w-fit flex justify-center items-center space-x-2  hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)] cursor-pointer">
+        <Link href={"mailto:mehtapratham49@gmail.com"} className="px-2 py-2 mt-2 w-fit flex justify-center items-center space-x-2  hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)] cursor-pointer">
           <FaArrowRight/>
           <p>Open for Freelance</p>
-        </div>
+        </Link>
       </div>
       <div>
         <div className="rounded overflow-hidden hover:scale-110 focus-within:scale-110 transition-transform duration-300">
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -20,19 +20,19 @@ const socialLinks = [
     name: "Peerlist",
   },
   {
-    link: "https://peerlist.io/usernam",
+    link: "mailto:mehtapratham49@gmail.com",
     icon: "/email.svg",
     name: "Email",
   },
 ];
- 
+
 const Socials = () => {
   return (
     <div className="mt-28">
       <div className="flex justify-center items-center gap-4">
-        {socialLinks.map(({ link, icon, name },idx) => (
+        {socialLinks.map(({ link, icon, name }, idx) => (
           <Link href={link} key={idx}>
-            <div className="px-2 py-2 md:w-32 sm:w-[106px] flex justify-center items-center space-x-2  hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)] ">
+            <div className="px-2 py-2 md:w-32 sm:w-[106px] flex justify-center items-center space-x-2 hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)]">
               <Image src={icon} width={20} height={20} alt="icon" />
               <p>{name}</p>
             </div>
