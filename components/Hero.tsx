5dba21f35f1ca39c02997013c793a7b7674829e8
Pratham mehta <mehtapratham49@gmail.com>
2025-03-25 19:51:12 +0530

    change the bio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,17 @@ const Hero = () => {
       <div>
         <h2 className="text-lg font-bold">Pratham | Full Stack Developer</h2>
         <div className="w-50 h-1 my-2 bg-white"></div>
-        <p className="text-md">
-          I love to work on full stack projects,Freelance Projects <br/> and to build
-          a robust applications i have worked on <br/> several freelance projects
+        <p className="text-md mb-4">
+          🚀 Passionate Full Stack Developer with a strong focus <br/> on building
+          robust and scalable applications. I love <br/> working on full-stack
+          projects, tackling complex challenges, <br/> and delivering high-quality
+          solutions. 💡
         </p>
-        <Link href={"mailto:mehtapratham49@gmail.com"} className="px-2 py-2 mt-2 w-fit flex justify-center items-center space-x-2  hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)] cursor-pointer">
-          <FaArrowRight/>
+        <Link
+          href={"mailto:mehtapratham49@gmail.com"}
+          className="px-2 py-2 mt-2 w-fit flex justify-center items-center space-x-2  hover:bg-[#292929] border border-r-0 border-b-0 border-zinc-600 bg-[#000000] text-white transition duration-500 font-bold text-sm shadow-[1px_1px_rgba(80,80,80),1px_1px_rgba(80,80,80),3px_3px_rgba(80,80,80),4px_4px_rgba(80,80,80)] cursor-pointer"
+        >
+          <FaArrowRight />
           <p>Open for Freelance</p>
         </Link>
       </div>
